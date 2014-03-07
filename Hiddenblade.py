6bc9ce0afebdad87c88235c0e76ca62b8f91e72a
Squayd <carroll.william.c@gmail.com>
2014-03-06 22:09:01 -0700

    Sequence thoughts and get_target method

--- Hiddenblade.py
+++ Hiddenblade.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #Hiddenblade
 #SMS based moderator program for Assassins
 #http://en.wikipedia.org/wiki/Assassin_(game)
@@ -12,17 +12,62 @@
 import random
 
 class Hiddenblade(object):
-	# The main game itself. Probably unnecessary, but I'm used to java so :p
-	## i think main game class makes sense, but probably dont need playerlist class
+	# The main game itself.
 	# TODO Add functionality to pass a filename. If a filename is passed, 
 	# check it for a valid game session and load it.
 	def __init__(self,players):
 		#run the actual game here
 		self.players = players
+		
+		#Thinking about the sequence
+		#Program gets run on my server box
+		#If it is given a file (either from an in-progress game or one we
+		#	create to start a game) it will load that game session and begin
+		#If it's not given a file, it will prompt for a player name and each
+		#	piece of information. It will display it to confirm before finally
+		#	adding the player, then ask if there is another player.
+		#When there are no more players to add, it will randomize the player
+		#	roster and create a list in the order of assigned targets.
+		#	Note - two lists.
+		#It will then wait until the system clock reaches midnight and send
+		#	an SMS to each player giving them the identity and information for
+		#	their target.
+		#It will then wait until it receives an SMS.
+		#	Any SMS will use the sending phone number to identify the player
+		#		sending it.
+		#	If SMS begins with 'target' reply with the player's target
+		#		information again.
+		#	if SMS begins with 'players' reply with the alphabetized names of 
+		#		the full player roster
+		#	if SMS begins with 'survivors' reply with an alphabetized list of
+		#		players still alive
+		#	if SMS begins with 'kill' remove the player's target from the list
+		#		and provide them information for their new target. Then
+		#		send SMS to all players saying who was elimnated
+		#			OR make it like hunger games - just say a player has been
+		#			eliminated and have a nightly recap SMS or something
+		#		If the player's new target is him/herself, announce them as the
+		#			victor, announce the player who had the most kills, exit.
+		#While waiting, possibly listen for console commands?
+		
 	def add_player(self,player):
 		self.players.append(player)
+		self.roster.append(player) #keep a separate list of all players
 	def kill_player(self,player):
 		self.players.remove(player)
+		
+	def get_target(self,player)
+		#returns the selected player's target as a Player() object. If the
+		#player is not found, returns None
+		for i in range(len(self.players))
+			#If the called player is the last player, wrap around to the first
+			if i == (len(self.players) - 1)
+				return self.players[0]
+			else if self.players[i] == player
+				return self.players[i+1]
+				else return None
+	#Do we need an analog to this function that returns the previous player?
+			
 	def save(self):
 		#save the game session to a file.
 		pass
@@ -37,6 +82,8 @@ class Hiddenblade(object):
 	def end_game(self):
 		#write final stats to a stats file and exit?
 		#maybe this can be called automatically from the main loop when the list reaches size 1
+		## yeah, or maybe when a player is eliminated, the assassin gets assigned a new target.
+		## end the game when the target becomes themself? I don't now, there are a billion ways.
 		pass
 		
 		
@@ -47,12 +94,17 @@ class Player(object):
 	### I think it would be best to just keep a list, and use existing list.remove function
 	### to delete players... player's target is next in list (first if they are last)
 	### player's hunter is previous in list (last if they are first)
+	###### Yeah, I got kinda stuck on linked list because it fits the idea of the way players relate to
+	###### each other, but it's not necessary when we'd have to implement it ourselves and if we're saving
+	###### to a flat file it's going to be a pain to rebuild that.
 	def __init__(self, name, status=True):
 		self.name = name
 		self.status = status
 		#maybe add address information? I think this info would be good to read from a file as well
 		#so that it's not dependent on making code changes every time the player list changes
-	
+		### I think address, workplace, possibly make and color of vehicle. For certain we'll need a 
+		### phone number. And any game tracking stats, like it could store a list of the names
+		### this player eliminated.
 
 if __name__ == '__main__':
 	
